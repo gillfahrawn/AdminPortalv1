b6c29e6f80adb14f9457d989670f6a4eb916aa15
gillfahrawn <88254192+gillfahrawn@users.noreply.github.com>
2025-12-02 20:25:21 -0500

    Update api.js
    For Render server (12/2, v1)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://adminportalv1.onrender.com'
+  /* || 'http://localhost:3001/api' ;*/
 
 const api = axios.create({
   baseURL: API_BASE_URL,
